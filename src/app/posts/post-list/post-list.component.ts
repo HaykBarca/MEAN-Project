1e4cc68b8afd4f45538301894703fe19c0ff89f7
HaykBarca <hayk19939563@gmail.com>
2019-01-09 00:27:52 +0400

    Added PostList, Header components and some material styles

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+    // posts = [
+    //     {title: 'First Title', content: 'First content'},
+    //     {title: 'Second Title', content: 'Second title'},
+    //     {title: 'Third Title', content: 'Third title'}
+    // ];
+    posts = [];
+
+}
