eca27f229521b34c7b96bd427fe1176add5a9052
HaykBarca <hayk19939563@gmail.com>
2019-01-10 22:13:34 +0400

    Added posts service and some methods in it

---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Post } from './post.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostsService {
+    private posts: Post[] = [];
+
+    getPosts() {
+        return [...this.posts];
+    }
+
+    addPost(title: string, content: string) {
+        this.posts.push({
+            title: title,
+            content: content
+        });
+    }
+}
