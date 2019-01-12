2676bda52799f620806ec1139315ad608a6b6d30
HaykBarca <hayk19939563@gmail.com>
2019-01-12 22:49:47 +0400

    Added some methods and sabled the app

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./model/post').postModel;
 
 const app = express();
+
+mongoose.connect('mongodb://HaykBarca:haykbarca19939563@ds255754.mlab.com:55754/angularapp')
+    .then(() => {
+        console.log('Connected Successfully');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,31 +27,37 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-        message: 'Added successfully'
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save().then(createdPost => {
+        console.log(post);
+        res.status(201).json({
+            message: 'Added successfully',
+            postId: createdPost._id
+        });
     });
 });
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        {
-            id: 'asdf4546sdf',
-            title: 'First post from Node',
-            content: 'First content from Node'
-        },
-        {
-            id: 'sdfsdfsdf5154',
-            title: 'Second post from Node',
-            content: 'Second content from Node'
-        }
-    ]
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Message with success',
+                posts: documents
+            })
+        });
+});
 
-    res.status(200).json({
-        message: 'Message with success',
-        posts
-    })
+app.delete('/api/posts/:id', (req, res, next) => {
+    Post.deleteOne({_id: req.params.id})
+        .then(
+            (result) => {
+                console.log(result);
+                res.status(200).json({ message: 'Post deleted.' });
+            }
+        )
 });
 
 module.exports = {app};
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -29,4 +29,8 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.postsSubscr.unsubscribe();
     }
 
+    onDelete(postId: string) {
+        this.postsService.deletePost(postId);
+    }
+
 }
